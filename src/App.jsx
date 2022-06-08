5286c0a80548bb67d3ed95841e05ab9054ef6bcd
manhMeoo <maot9011@gmail.com>
2022-06-08 12:31:36 +0700

    cmt bug

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-import { Route, Routes } from "react-router-dom";
-import "bootstrap/dist/css/bootstrap.css";
-import "bootstrap/dist/js/bootstrap.js";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.min.js";
 import "bootstrap-icons/font/bootstrap-icons.css";
 import Home from "./pages/home/Home";
 import Products from "./pages/products/Products";
 import "./App.css";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
--- src/pages/home/banner/Banner.jsx
+++ src/pages/home/banner/Banner.jsx
@@ -10,75 +10,75 @@ import Banner2 from "../../../assets/imgs/banner2.jpg";
 import axios from "axios";
 
 const Banner = () => {
-  const [isLoading, setLoading] = useState(false);
-  const [Products, setProducts] = useState();
+  // const [isLoading, setLoading] = useState(false);
+  // const [Products, setProducts] = useState();
 
-  useEffect(() => {
-    setLoading(true);
-    try {
-      axios
-        .get(`http://localhost:3000/products`)
-        .then((res) => setProducts(res.data));
-      setLoading(false);
-    } catch (error) {
-      console.log(error);
-    }
-  }, []);
-  setLoading(false);
-  if (isLoading)
-    return (
-      <>
-        <h1>loading...</h1>
-      </>
-    );
-  else
-    return (
-      <>
-        <Swiper
-          speed={1500}
-          spaceBetween={30}
-          centeredSlides={true}
-          autoplay={{
-            delay: 5000,
-            disableOnInteraction: false,
-          }}
-          loop={true}
-          pagination={{
-            clickable: true,
-          }}
-          navigation={true}
-          modules={[Autoplay, Pagination, Navigation]}
-          className="mySwiper swiper bannerSwipper"
-        >
-          <SwiperSlide>
-            <div className="swiper-slide">
-              <img src={Banner2} alt="banner" />
-              <div className="campain-details campain1">
-                <div className="campain-text season">New Arrivals</div>
-                <div className="campain-text main">New Collection</div>
-                <div className="campain-text sub">
-                  Up to 90% off on Accessories
-                </div>
-                <button className="campain-text-btn">SHOP NOW</button>
+  // useEffect(() => {
+  //   setLoading(!isLoading);
+  //   try {
+  //     axios
+  //       .get(`http://localhost:3000/products`)
+  //       .then((res) => setProducts(res.data));
+  //     setLoading(!isLoading);
+  //   } catch (error) {
+  //     console.log(error);
+  //   }
+  // }, []);
+  // setLoading(false);
+  // if (isLoading)
+  //   return (
+  //     <>
+  //       <h1>loading...</h1>
+  //     </>
+  //   );
+  // else
+  return (
+    <>
+      <Swiper
+        speed={1500}
+        spaceBetween={30}
+        centeredSlides={true}
+        autoplay={{
+          delay: 5000,
+          disableOnInteraction: false,
+        }}
+        loop={true}
+        pagination={{
+          clickable: true,
+        }}
+        navigation={true}
+        modules={[Autoplay, Pagination, Navigation]}
+        className="mySwiper swiper bannerSwipper"
+      >
+        <SwiperSlide>
+          <div className="swiper-slide">
+            <img src={Banner2} alt="banner" />
+            <div className="campain-details campain1">
+              <div className="campain-text season">New Arrivals</div>
+              <div className="campain-text main">New Collection</div>
+              <div className="campain-text sub">
+                Up to 90% off on Accessories
               </div>
+              <button className="campain-text-btn">SHOP NOW</button>
             </div>
-          </SwiperSlide>
-          <SwiperSlide>
-            <div className="swiper-slide">
-              <img src={Banner5} alt="banner" />
-              <div className="campain-details campain1">
-                <div className="campain-text season">New Arrivals</div>
-                <div className="campain-text main">New Collection</div>
-                <div className="campain-text sub">
-                  Up to 90% off on Accessories
-                </div>
-                <button className="campain-text-btn">SHOP NOW</button>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className="swiper-slide">
+            <img src={Banner5} alt="banner" />
+            <div className="campain-details campain1">
+              <div className="campain-text season">New Arrivals</div>
+              <div className="campain-text main">New Collection</div>
+              <div className="campain-text sub">
+                Up to 90% off on Accessories
               </div>
+              <button className="campain-text-btn">SHOP NOW</button>
             </div>
-          </SwiperSlide>
-        </Swiper>
-      </>
-    );
+          </div>
+        </SwiperSlide>
+      </Swiper>
+    </>
+  );
 };
 
 export default Banner;
